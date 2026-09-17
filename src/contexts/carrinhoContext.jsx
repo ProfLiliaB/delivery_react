9c30d97e22f5a78723af43e2f2ab05f02c34aea3
LiBorges <liliapds@gmail.com>
2026-09-17 18:14:39 -0300

    Atualização de arquivos

--- src/contexts/carrinhoContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-// O Contexto é exportado apenas para o Provider conectá-lo
-export const CarrinhoContext = createContext();
-
-// O Hook é exportado para as telas consumirem os dados
-export function useCarrinho() {
-    return useContext(CarrinhoContext);
-}
